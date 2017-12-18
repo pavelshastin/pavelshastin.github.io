c88beaa4d3af50bdd9c4e95d7c206ab52ec5d106
Pavel <pavelshastin@yandex.ru>
2017-12-18 16:16:08 +0400

    Add files via upload

--- js/svg-or-png.js
+++ js/svg-or-png.js
@@ -1,1 +1,53 @@
-function getXmlHttp(){var e;try{e=new ActiveXObject("Msxml2.XMLHTTP")}catch(t){try{e=new ActiveXObject("Microsoft.XMLHTTP")}catch(t){e=!1}}return e||"undefined"==typeof XMLHttpRequest||(e=new XMLHttpRequest),e}if(Modernizr.svg){var elem=document.createElement("svg");elem.setAttribute("xmlns","http://www.w3.org/2000/svg"),elem.setAttribute("style","display: none;");var req=getXmlHttp();req.open("GET","./images/icons.svg",!0),req.onreadystatechange=function(){4==req.readyState&&200==req.status&&(elem.innerHTML=req.responseText,document.body.prepend(elem))},req.send(null)}else{var link=document.createElement("link");link.setAttribute("href","./css/sprites.css"),link.setAttribute("type","text/css"),link.setAttribute("rel","stylesheet"),document.head.appendChild(link)}
+
+if(Modernizr.svg) { 
+
+	function getXmlHttp(){
+	  var xmlhttp;
+	  try {
+	    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+	  } catch (e) {
+	    try {
+	      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	    } catch (E) {
+	      xmlhttp = false;
+	    }
+	  }
+	  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+	    xmlhttp = new XMLHttpRequest();
+	  }
+	  return xmlhttp;
+	}
+
+
+
+	
+	var elem = document.createElement("svg"); 
+		elem.setAttribute("xmlns", "http://www.w3.org/2000/svg"); 
+		elem.setAttribute("style", "display: none;"); 
+		 
+	var req = getXmlHttp();
+	req.open('GET', './images/icons.svg', true); 
+	
+	req.onreadystatechange = function() {
+		  if (req.readyState == 4) {
+		     if(req.status == 200) {
+		     	elem.innerHTML = req.responseText;
+		       	document.body.prepend(elem);
+			 }
+		  }
+		};
+	req.send(null); 	
+
+} else {
+
+	var link = document.createElement('link');
+		link.setAttribute('href', './css/sprites.css');
+		link.setAttribute('type', 'text/css');
+		link.setAttribute('rel', 'stylesheet');
+
+	document.head.appendChild(link);
+}
+
+
+
+
